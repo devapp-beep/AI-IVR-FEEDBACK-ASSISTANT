9048478a2185a8d5e2baacf2ec506754b886a7e7
ankitb5-tech <ankit.b5@cynetcorp.com>
2025-12-09 12:47:48 -0500

    add: recruiter checking feature

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,10 @@ def create_app():
     try:
         from .routes.health_controller import health_bp
         from .routes.vapi_feedback_controller import vapi_feedback_bp
+        from .routes.vapi_recruiter_info import vapi_recruiter_info_bp
         app.register_blueprint(health_bp)          # -> /health
         app.register_blueprint(vapi_feedback_bp)     # -> /user_feedback
-        
+        app.register_blueprint(vapi_recruiter_info_bp)     # -> /recruiter_info
         # If you want an /api prefix: app.register_blueprint(vapi_bp, url_prefix="/api")
     except Exception as e:
         print("\n[IMPORT ERROR] Couldn't register blueprints:", e, "\n")
